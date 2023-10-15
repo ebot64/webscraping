c7e0daf60a061670fe79c2b1e6420612acf729ef
Tobechukwu Ezenachukwu <ebot64@yahoo.com>
2023-10-15 22:51:22 +0100

    Performed filtering for movies after Year 2000

--- webscraping.py
+++ webscraping.py
@@ -4,10 +4,10 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 url = 'https://web.archive.org/web/20230902185655/https://en.everybodywiki.com/100_Most_Highly-Ranked_Films'
-db_name = 'Movies.db'
-table_name = 'Top_50'
-csv_path = 'top_50_films.csv'
-df = pd.DataFrame(columns=["Average Rank","Film","Year"])
+db_name = 'Movies2.db'
+table_name = 'Top_25'
+csv_path = 'top_25_films.csv'
+df = pd.DataFrame(columns=["Average Rank","Film","Year", "Rotten Tomatoes' Top 100"])
 count = 0
 
 html_page = requests.get(url).text
@@ -17,15 +17,17 @@ tables = data.find_all('tbody')
 rows = tables[0].find_all('tr')
 
 for row in rows:
-    if count<50:
+    if count<25:
         col = row.find_all('td')
         if len(col)!=0:
-            data_dict = {"Average Rank": col[0].contents[0],
-                         "Film": col[1].contents[0],
-                         "Year": col[2].contents[0]}
-            df1 = pd.DataFrame(data_dict, index=[0])
-            df = pd.concat([df,df1], ignore_index=True)
-            count+=1
+            if col[2].contents[0]>="2000":
+                data_dict = {"Average Rank": col[0].contents[0],
+                            "Film": col[1].contents[0],
+                            "Year": col[2].contents[0],
+                            "Rotten Tomatoes' Top 100": col[3].contents[0]}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
+                count+=1
     else:
         break
 
